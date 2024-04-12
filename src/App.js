a1a3416e8e5f8138e5310e458852de04dbbda5b0
Илья Колбин <ilakolbin@MacBook-Pro-2.local>
2024-04-12 16:49:14 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import {Route, Routes} from "react-router-dom";
+
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import NotFound from "./pages/NotFound";
+import FullPizza from "./pages/FullPizza";
+
+import './scss/app.scss'
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+			<div className="wrapper">
+				<Header/>
+				<div className="content">
+					<div className="container">
+						<Routes>
+							<Route path='/' element={<Home/>}/>
+							<Route path='/cart' element={<Cart/>}/>
+							<Route path='/pizza/:id' element={<FullPizza/>}/>
+							<Route path='*' element={<NotFound/>}/>
+						</Routes>
+					</div>
+				</div>
+			</div>
+	);
 }
 
 export default App;
